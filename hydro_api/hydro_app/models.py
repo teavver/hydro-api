ff8d1c2cbfdae6fbc2da57e06ff74b8b404e1d6f
teavver <jerzyx11@gmail.com>
2024-05-30 14:46:57 +0200

    chore: initial django app structure

--- hydro_api/hydro_app/models.py
+++ hydro_api/hydro_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class HydroponicSystem(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Measurement(models.Model):
+    system = models.ForeignKey(HydroponicSystem, on_delete=models.CASCADE)
+    ph = models.FloatField()
+    temperature = models.FloatField()
+    tds = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
--- hydro_api/hydro_app/views.py
+++ hydro_api/hydro_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import HydroponicSystem, Measurement
+from .serializers import HydroponicSystemSerializer, MeasurementSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class HydroponicSystemViewSet(viewsets.ModelViewSet):
+    queryset = HydroponicSystem.objects.all()
+    serializer_class = HydroponicSystemSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MeasurementViewSet(viewsets.ModelViewSet):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+    permission_classes = [IsAuthenticated]
