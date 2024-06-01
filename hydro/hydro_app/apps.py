4ca42de0688620949369b03d7f20d8f8a3857a65
teavver <jerzyx11@gmail.com>
2024-06-01 18:08:03 +0200

    chore: add PEP8 auto formatter

--- hydro/hydro_app/apps.py
+++ hydro/hydro_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class HydroAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hydro_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hydro_app"
